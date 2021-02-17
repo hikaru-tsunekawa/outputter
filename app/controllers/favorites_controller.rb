34de3fb00a98beca9f3f20232021f36523a93c4c
EC2 Default User <ec2-user@ip-172-31-62-86.ec2.internal>
2021-02-17 02:22:02 +0000

    bootstrap ver2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     note = Note.find(params[:note_id])
     favorite = current_user.favorites.new(note_id: note.id)
     favorite.save
-    redirect_to note_path(note)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     note = Note.find(params[:note_id])
     favorite = current_user.favorites.find_by(note_id: note.id)
     favorite.destroy
-    redirect_to note_path(note)
+    redirect_back(fallback_location: root_path)
   end
 end
